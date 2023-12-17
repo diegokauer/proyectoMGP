65d490bd8893537af319f23f51903d009eff5d0f
Felipe-mendezp <70618790+Felipe-mendezp@users.noreply.github.com>
2023-12-17 20:58:05 -0300

    Update vqvae.py

--- models/vqvae.py
+++ models/vqvae.py
@@ -209,11 +209,19 @@ class VQVAEModel(object):
                 best_loss = avg_loss
                 self.save()
 
-    def save(self):
-        torch.save(self.model.state_dict(), '../checkpoint/vqvae_checkpoint')
+    def save(self, path=None):
+        if path == None:
+            torch.save(self.model.state_dict(), './checkpoint/vqvae_checkpoint')
+        else:
+            torch.save(self.model.state_dict(), path)
 
-    def load(self):
-        self.model.load_state_dict(
-            torch.load('../checkpoint/vqvae_checkpoint', map_location=torch.device(self.device))
-        )
+    def load(self, path=None):
+        if path == None:
+            self.model.load_state_dict(
+                torch.load('./checkpoint/vqvae_checkpoint', map_location=torch.device(self.device))
+            )
+        else:
+            self.model.load_state_dict(
+                torch.load(path, map_location=torch.device(self.device))
+            )
         print('Modelo Cargado')
